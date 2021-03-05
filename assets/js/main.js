f39c6d07fe8fd945bf6831f65d3222ff1e99a387
Leonardo <leonardo.bongianni@gmail.com>
2021-03-05 19:38:56 +0100

    Stampo a schermo

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,20 @@ for (var i = 1; i <= 100; i++) {
 // se il numero è sia multiplo di 3 che di 5 allora stampa Fizz Buzz 🐱‍👤
   if ((i%3 == 0) && (i%5 == 0)) {
     console.log("Fizz Buzz");
+    document.getElementById('lista').innerHTML += "<li>" + "Fizz Buzz" + "</li>"
     // altrimenti se è solo multiplo di 3 stampa Fizz 🐱‍👤
     } else if (i%3 == 0) {
       console.log("Fizz");
+      document.getElementById('lista').innerHTML += "<li>" + "Fizz" + "</li>"
+
     // altrimenti se è solo multiplo di 5 stampa Buzz 🐱‍👤
     } else if (i%5 == 0) {
       console.log("Buzz");
+      document.getElementById('lista').innerHTML += "<li>" + "Buzz" + "</li>"
+
     // altrimenti stampa il numero 🐱‍👤
     } else {
       console.log(i);
+      document.getElementById('lista').innerHTML += "<li>" + i + "</li>"
     }
 }
