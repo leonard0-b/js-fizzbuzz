47766c82959881b7e882da834806d553b860c6cc
Leonardo <leonardo.bongianni@gmail.com>
2021-03-06 23:41:21 +0100

    Esercizio Bonus Completo

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -30,12 +30,12 @@ for (var i = 1; i <= 10; i++) {
 
 // seleziono la classe bottone da html 🐱‍👤
 var bottone = document.querySelectorAll(".button");
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "dieci" diventando un array 🐱‍👤
 var fizz10 = document.getElementsByClassName("dieci");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz10.length; i++) {
     fizz10[i].style.color = "#6d5c7e";
   }
 });
@@ -65,12 +65,12 @@ for (var i = 11; i <= 20; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "venti" diventando un array 🐱‍👤
 var fizz20 = document.getElementsByClassName("venti");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz20.length; i++) {
     fizz20[i].style.color = "#6d5c7e";
   }
 });
@@ -100,12 +100,12 @@ for (var i = 21; i <= 30; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "trenta" diventando un array 🐱‍👤
 var fizz30 = document.getElementsByClassName("trenta");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz30.length; i++) {
     fizz30[i].style.color = "#6d5c7e";
   }
 });
@@ -135,12 +135,12 @@ for (var i = 31; i <= 40; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "quaranta" diventando un array 🐱‍👤
 var fizz40 = document.getElementsByClassName("quaranta");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz40.length; i++) {
     fizz40[i].style.color = "#6d5c7e";
   }
 });
@@ -170,12 +170,12 @@ for (var i = 41; i <= 50; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "cinquanta" diventando un array 🐱‍👤
 var fizz50 = document.getElementsByClassName("cinquanta");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz50.length; i++) {
     fizz50[i].style.color = "#6d5c7e";
   }
 });
@@ -205,12 +205,12 @@ for (var i = 51; i <= 60; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "sessanta" diventando un array 🐱‍👤
 var fizz60 = document.getElementsByClassName("sessanta");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz60.length; i++) {
     fizz60[i].style.color = "#6d5c7e";
   }
 });
@@ -240,12 +240,12 @@ for (var i = 61; i <= 70; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "settanta" diventando un array 🐱‍👤
 var fizz70 = document.getElementsByClassName("settanta");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz70.length; i++) {
     fizz70[i].style.color = "#6d5c7e";
   }
 });
@@ -275,12 +275,12 @@ for (var i = 71; i <= 80; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "ottanta" diventando un array 🐱‍👤
 var fizz80 = document.getElementsByClassName("ottanta");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz80.length; i++) {
     fizz80[i].style.color = "#6d5c7e";
   }
 });
@@ -312,12 +312,12 @@ for (var i = 81; i <= 90; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
+// creo variabile che racchiuda gli li con classe "novanta" diventando un array 🐱‍👤
 var fizz90 = document.getElementsByClassName("novanta");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
+  for (var i = 0; i < fizz90.length; i++) {
     fizz90[i].style.color = "#6d5c7e";
   }
 });
@@ -346,12 +346,12 @@ for (var i = 91; i <= 100; i++) {
     }
 }
 
-// creo variabile che racchiuda gli li con classe cento diventando un array 🐱‍👤
-var fizz = document.getElementsByClassName("cento");
+// creo variabile che racchiuda gli li con classe "cento" diventando un array 🐱‍👤
+var fizz00 = document.getElementsByClassName("cento");
 // creo un evento al click 🐱‍👤
 bottone[0].addEventListener("click", function(e) {
   // ciclo per cambiare a ogni elemento dell'array il colore 🐱‍👤
-  for (var i = 0; i < fizz.length; i++) {
-    fizz[i].style.color = "#6d5c7e";
+  for (var i = 0; i < fizz00.length; i++) {
+    fizz00[i].style.color = "#6d5c7e";
   }
 });
